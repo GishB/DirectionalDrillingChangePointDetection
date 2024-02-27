6d981fba16c5611ea1f881d55a432a34856480af
GishB <SamofalovWORK@yandex.ru>
2024-02-28 00:35:16 +0300

    fix CPs function based on residuals dp method

--- models/ProbabilityBased.py
+++ models/ProbabilityBased.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from collections import namedtuple
 
 import sys
 
@@ -21,7 +20,8 @@ class KalmanFilter(WindowSizeSelection, Filter):
 
     def __init__(self, df: pd.DataFrame = None,
                  target_column: str = None,
-                 window: int = 10,
+                 kalman_window: int = None,
+                 queue_window: int = None,
                  is_cps_filter_on: bool = True,
                  is_quantile_threshold: bool = False,
                  threshold_quantile_coeff: float = 0.91,
@@ -29,28 +29,39 @@ class KalmanFilter(WindowSizeSelection, Filter):
                  kalman_power_coeff: float = 0.9):
         self.df = df
         self.target_column = target_column
-        self.window = window
+        self.kalman_window = kalman_window
+        self.queue_window = queue_window
         self.threshold_quantile_coeff = threshold_quantile_coeff
         self.is_cps_filter_on = is_cps_filter_on
         self.threshold_std_coeff = threshold_std_coeff
         self.is_quantile_threshold = is_quantile_threshold
         self.kalman_power_coeff = kalman_power_coeff
 
-        if window is None:
+        if kalman_window is None:
             super().__init__(time_series=df[target_column].values)
-            self.window = self.runner_wss()[0]
+            self.kalman_window = self.runner_wss()[0]
+
+        if queue_window is None:
+            self.queue_window = kalman_window*2
+
+        if kalman_window <= 10:
+            raise NotImplementedError("This algorithm highly like will raise an error because your initial df shape "
+                                      "less then 10 points")
+
+        if target_column is None:
+            raise ValueError("You must to define target column!")
 
     @staticmethod
     def get_array_info(data: np.array) -> tuple[float, float]:
         """ Get gaussian stats based on an array of values.
 
         Args:
-            data:
+            data: slice of time series values.
 
         Returns:
             gaussian stats tuple as mean and std values
         """
-        gaussian_info = data.mean(), data.var()
+        gaussian_info = np.mean(data), np.std(data)
         return gaussian_info
 
     def gaussian_multiply(self, g1: tuple[float, float], g2: tuple[float, float]) -> tuple[float, float]:
@@ -74,7 +85,8 @@ class KalmanFilter(WindowSizeSelection, Filter):
         variance = (g1[1] * g2[1]) / (g1[1] + g2[1])
         return mean, variance
 
-    def forecast(self, mean_gauss: float, std_gauss: float) -> np.ndarray:
+    @staticmethod
+    def forecast(mean_gauss: float, std_gauss: float) -> float:
         """ forecast next values based on estimated gaussian coefficient.
 
         Args:
@@ -82,9 +94,9 @@ class KalmanFilter(WindowSizeSelection, Filter):
             std_gauss: expected std value.
 
         Returns:
-            normal generated array based on expected mean and std.
+            normal generated value based on expected mean and std.
         """
-        return np.random.normal(loc=mean_gauss, scale=std_gauss, size=self.window)
+        return np.random.normal(loc=mean_gauss, scale=std_gauss)
 
     @staticmethod
     def update(mult_gaussian: tuple[float, float], actual_gaussian: tuple[float, float]) -> tuple[float, float]:
@@ -108,23 +120,29 @@ class KalmanFilter(WindowSizeSelection, Filter):
         Returns:
             array of residuals between generated data and real values.
         """
-        gaussian_forecasted_list = []
+        gaussian_forecasted_list = [val for val in self.df[self.target_column][:self.kalman_window]]
         gaussian_likelihood = self.get_array_info(self.df[self.target_column].values)
-        for generation_epoch in range(0, self.df.shape[0] // self.window + 1):
-            predicted_tmp = self.forecast(mean_gauss=gaussian_likelihood[0],
-                                          std_gauss=np.sqrt(gaussian_likelihood[1]))
-            gaussian_forecasted_list.extend(predicted_tmp)
-            actual_array = self.df[self.target_column].iloc[
-                           self.window * generation_epoch:self.window * (generation_epoch + 1)].values
-            if actual_array.shape[0] != 0:
-                actual_gaussian = self.get_array_info(actual_array)
-                mult_gaussian = self.gaussian_multiply(
-                    g1=gaussian_likelihood,
-                    g2=actual_gaussian
-                )
-                gaussian_likelihood = self.update(mult_gaussian, actual_gaussian)
-        gaussian_forecasted_list = np.array(gaussian_forecasted_list)[:self.df.shape[0]]
-        return gaussian_forecasted_list
+        dp = [val for val in self.df[self.target_column][:self.kalman_window]]
+        for generation_epoch in range(self.kalman_window, self.df.shape[0]):
+            gaussian_forecasted_list.append(self.forecast(mean_gauss=gaussian_likelihood[0],
+                                                          std_gauss=np.sqrt(gaussian_likelihood[1])))
+            actual_gaussian = self.get_array_info(dp)
+            mult_gaussian = self.gaussian_multiply(
+                g1=gaussian_likelihood,
+                g2=actual_gaussian
+            )
+            gaussian_likelihood = self.update(mult_gaussian, actual_gaussian)
+            dp.pop(0)
+            dp.append(self.df[self.target_column][generation_epoch])
+        return np.array(gaussian_forecasted_list)
+
+    def get_residuals(self):
+        """ Calculate residuals between filtered time series and original.
+
+        Returns:
+            array of residuals.
+        """
+        return self.df[self.target_column].values - self.get_full_forecast()
 
     def predict(self) -> np.ndarray:
         """ Change Point Detection based on failure statistics.
@@ -136,16 +154,22 @@ class KalmanFilter(WindowSizeSelection, Filter):
         Returns:
             array of binary change points labels.
         """
-        residuals = abs(self.df[self.target_column].values - self.get_full_forecast()) ** 2
-        if self.is_quantile_threshold:
-            quantile_val = np.quantile(residuals, q=self.threshold_quantile_coeff)
-            cps_list = [1 if val > quantile_val else 0 for val in residuals]
-        else:
-            mean_val = residuals.mean()
-            std_val = residuals.std() * self.threshold_std_coeff
-            cps_list = [1 if val > (mean_val + std_val) else 0 for val in residuals]
+        residuals = abs(self.get_residuals())
+        dp = [val for val in residuals[:self.kalman_window]]
+        cps_list = [0 for ind in range(self.kalman_window)]
+        mean_val = np.mean(dp)
+        std_val = np.std(dp) * self.threshold_std_coeff
+        for val in residuals[self.kalman_window:]:
+            if val > (mean_val + std_val) or val < (mean_val - std_val):
+                cps_list.append(1)
+            else:
+                cps_list.append(0)
+            dp.append(val)
+            dp.pop(0)
+            mean_val = np.mean(dp)
+            std_val = np.std(dp) * self.threshold_std_coeff
         if self.is_cps_filter_on:
-            cps_list = self.queue(time_series=cps_list, queue_window=self.window, reversed=False)
+            cps_list = self.queue(time_series=cps_list, queue_window=self.queue_window, reversed=False)
         return np.array(cps_list)
 
 
