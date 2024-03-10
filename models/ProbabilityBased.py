10a78ce4362613bc92a5fb9366e8230d9d8211c3
GishB <SamofalovWORK@yandex.ru>
2024-03-11 01:42:08 +0300

    edit inherit based on Constructor idea

--- models/ProbabilityBased.py
+++ models/ProbabilityBased.py
@@ -5,22 +5,22 @@ import sys
 
 sys.path.append("..")
 from utils.hyperparameters.WSSAlgorithms import WindowSizeSelection
-from utils.DataTransformers import Filter
+from models.ModelConstructors import ChangePointDetectionConstructor
 
 
-class KalmanFilter(WindowSizeSelection, Filter):
+class KalmanFilter(WindowSizeSelection, ChangePointDetectionConstructor):
     """ Idea is to find data deviations based on Kalman extrapolation for nearest data.
 
     Attributes:
         df: pandas dataframe with target data.
         target_column: column which should be checked.
-        window: expected window for analysis.
+        sequence_window: expected window for analysis.
         threshold_quantile_coeff: expected deviation between generated and original data.
     """
 
     def __init__(self, df: pd.DataFrame = None,
                  target_column: str = None,
-                 kalman_window: int = None,
+                 sequence_window: int = None,
                  queue_window: int = None,
                  is_cps_filter_on: bool = True,
                  is_quantile_threshold: bool = False,
@@ -29,7 +29,7 @@ class KalmanFilter(WindowSizeSelection, Filter):
                  kalman_power_coeff: float = 0.9):
         self.df = df
         self.target_column = target_column
-        self.kalman_window = kalman_window
+        self.sequence_window = sequence_window
         self.queue_window = queue_window
         self.threshold_quantile_coeff = threshold_quantile_coeff
         self.is_cps_filter_on = is_cps_filter_on
@@ -37,14 +37,14 @@ class KalmanFilter(WindowSizeSelection, Filter):
         self.is_quantile_threshold = is_quantile_threshold
         self.kalman_power_coeff = kalman_power_coeff
 
-        if kalman_window is None:
+        if sequence_window is None:
             super().__init__(time_series=df[target_column].values)
-            self.kalman_window = self.runner_wss()[0]
+            self.sequence_window = self.runner_wss()[0]
 
         if queue_window is None:
-            self.queue_window = kalman_window*2
+            self.queue_window = sequence_window*2
 
-        if kalman_window <= 10:
+        if sequence_window <= 10:
             raise NotImplementedError("This algorithm highly like will raise an error because your initial df shape "
                                       "less then 10 points")
 
@@ -120,10 +120,10 @@ class KalmanFilter(WindowSizeSelection, Filter):
         Returns:
             array of residuals between generated data and real values.
         """
-        gaussian_forecasted_list = [val for val in self.df[self.target_column][:self.kalman_window]]
+        gaussian_forecasted_list = [val for val in self.df[self.target_column][:self.sequence_window]]
         gaussian_likelihood = self.get_array_info(self.df[self.target_column].values)
-        dp = [val for val in self.df[self.target_column][:self.kalman_window]]
-        for generation_epoch in range(self.kalman_window, self.df.shape[0]):
+        dp = [val for val in self.df[self.target_column][:self.sequence_window]]
+        for generation_epoch in range(self.sequence_window, self.df.shape[0]):
             gaussian_forecasted_list.append(self.forecast(mean_gauss=gaussian_likelihood[0],
                                                           std_gauss=np.sqrt(gaussian_likelihood[1])))
             actual_gaussian = self.get_array_info(dp)
@@ -136,43 +136,13 @@ class KalmanFilter(WindowSizeSelection, Filter):
             dp.append(self.df[self.target_column][generation_epoch])
         return np.array(gaussian_forecasted_list)
 
-    def get_residuals(self):
+    def get_distances(self):
         """ Calculate residuals between filtered time series and original.
 
         Returns:
             array of residuals.
         """
         return self.df[self.target_column].values - self.get_full_forecast()
-
-    def predict(self) -> np.ndarray:
-        """ Change Point Detection based on failure statistics.
-
-        Notes:
-            1. By default, we expect that threshold value can be found via quantile value due the fact that CPs shape are
-            less time series shape.
-            2. Keep in mind that queue window algorithm always saves the first anomaly as true result and
-             drop others based on queue window range.
-
-        Returns:
-            array of binary change points labels.
-        """
-        residuals = abs(self.get_residuals())
-        dp = [val for val in residuals[:self.kalman_window]]
-        cps_list = [0 for ind in range(self.kalman_window)]
-        mean_val = np.mean(dp)
-        std_val = np.std(dp) * self.threshold_std_coeff
-        for val in residuals[self.kalman_window:]:
-            if val > (mean_val + std_val) or val < (mean_val - std_val):
-                cps_list.append(1)
-            else:
-                cps_list.append(0)
-            dp.append(val)
-            dp.pop(0)
-            mean_val = np.mean(dp)
-            std_val = np.std(dp) * self.threshold_std_coeff
-        if self.is_cps_filter_on:
-            cps_list = self.queue(time_series=cps_list, queue_window=self.queue_window, reversed=False)
-        return np.array(cps_list)
 
 
 if __name__ == "__main__":
@@ -181,5 +151,6 @@ if __name__ == "__main__":
 
     data = SinusoidWaves(length_data=2000, cps_number=5, white_noise_level="min").get()
 
-    model = KalmanFilter(df=data, target_column="x", window=250, threshold_quantile_coeff=0.95, is_cps_filter_on=True)
+    model = KalmanFilter(df=data, target_column="x", queue_window=250,
+                         threshold_quantile_coeff=0.95, is_cps_filter_on=True)
     cps_pred = model.predict()
