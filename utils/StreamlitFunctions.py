9edbd0fe3a7d92b9b7e6c7128ce6e9afadf0c942
GishB <SamofalovWORK@yandex.ru>
2024-03-19 01:06:26 +0300

    add init model params

--- utils/StreamlitFunctions.py
+++ utils/StreamlitFunctions.py
@@ -1,14 +1,60 @@
 import pandas as pd
-from typing import Optional
+from typing import Optional, Dict, Any
 import sys
+import streamlit as st
 
 sys.path.append("../..")
 
 from data.SythData import LinearSteps, SinusoidWaves
 
 
-def init_model_params(option, model):
-    return None
+def init_model_params(model_name: str) -> Dict[str: Any]:
+    """ Select params for custom model from module models.
+
+    Returns:
+        dict of params for custom model.
+    """
+    params = {
+        "is_z_normalization": st.checkbox("is_z_normalization"),
+        "is_cps_filter_on": st.checkbox("is_cps_filter_on"),
+        "is_squared_residual": st.checkbox("is_squared_residual"),
+        "is_cumsum_applied": st.checkbox("is_cumsum_applied"),
+        "queue_window": st.slider('queue_window', 10, 100, 10),
+        "threshold_std_coeff": st.slider('threshold_str_coeff', 2.1, 5.1, 3.1)
+    }
+    if model_name == "Singular Sequence Decomposition":
+        params["n_components"] = st.slider('n_components PCA', 1, 3, 2),
+    elif model_name == "Kalman Filter":
+        pass
+    return params
+
+
+def init_data_loader_params() -> Dict[str: Any]:
+    """ Init params to loan syth data.
+
+    Notes:
+        length_data should be equally split based on cps_number.
+
+    Examples:
+        1. length_data = 1000, cps_number=[2, 4, 10, 20, 50]
+        2. length_data = 100, cps_number=[2, 4]
+
+    Warnings:
+        cps_number should split data equally!
+
+    Returns:
+        dict of params to generate syth data.
+    """
+    params = {
+        "length_data": st.slider('length_data', 100, 1000, 500),
+        "cps_number": st.slider('cps_number', 2, 100, 500),
+        "white_noise_level": st.radio(
+            "What's noise level you want to select",
+            ["min", "default", "max"],
+            index=0,
+        )
+    }
+    return params
 
 
 def data_loader(option: str, params: dict) -> Optional[pd.DataFrame]:
