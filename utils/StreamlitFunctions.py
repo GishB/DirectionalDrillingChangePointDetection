3131413cb6dd855b8deff734ddf1602b16be23f8
GishB <SamofalovWORK@yandex.ru>
2024-03-19 00:40:49 +0300

    add data_loader default

--- utils/StreamlitFunctions.py
+++ utils/StreamlitFunctions.py
@@ -1,6 +1,34 @@
+import pandas as pd
+from typing import Optional
+import sys
+
+sys.path.append("../..")
+
+from data.SythData import LinearSteps, SinusoidWaves
+
+
 def init_model_params(option, model):
     return None
 
 
-def data_loader(option):
-    return None
+def data_loader(option: str, params: dict) -> Optional[pd.DataFrame]:
+    """ Load data based on option query.
+
+    Args:
+        option: option name.
+        params: dict of params to generate syth data if any.
+
+    Returns:
+        dataframe or None in case of none option.
+    """
+    if option == "Syth-Steps":
+        data = LinearSteps(length_data=params.get("length_data"),
+                           cps_number=params.get("cps_number"),
+                           white_noise_level=params.get("white_noise_level")).get()
+    elif option == 'Syth-Sinusoid':
+        data = SinusoidWaves(length_data=params.get("length_data"),
+                             cps_number=params.get("cps_number"),
+                             white_noise_level=params.get("white_noise_level")).get()
+    else:
+        data = None
+    return data
