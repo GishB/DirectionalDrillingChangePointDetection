4f1ee8b881678ad4f330756410413ed1b531e344
GishB <SamofalovWORK@yandex.ru>
2024-03-19 01:35:15 +0300

    default pipline to load data and plot it as well as select model params

--- app/main.py
+++ app/main.py
@@ -3,24 +3,23 @@ import utils.StreamlitFunctions as useful
 
 st.title('Change Point Detection app.')
 
-st.sidebar.header('UI Model pipline')
+st.sidebar.header('UI Model pipeline')
 option_model = st.sidebar.selectbox(
     'Select CPD model',
-    ('SingularSequenceTransformation', 'Kalman'))
+    ("Singular Sequence Decomposition", "Kalman Filter"))
+useful.init_model_params(model_name=option_model)
 
 option_data = st.sidebar.selectbox(
     'Select dataset',
-    ('None', 'Custom', 'SythData', 'CloudData'))
+    ("Syth-Steps", "Syth-Sinusoid"))
 
 if option_data != "None":
-    useful.data_loader(option=option_data)
+    data_loader_params = useful.init_data_loader_params()
+    df = useful.data_loader(option=option_data, params=data_loader_params)
+    useful.data_info()
+    useful.data_plot()
 
-option_model = st.sidebar.selectbox(
-    "Select hyperparameters",
-    ("Default", "Custom")
-)
 
-if option_model != "Default":
-    useful.init_model_params(option=option_model, model=option_model)
+
 
 
