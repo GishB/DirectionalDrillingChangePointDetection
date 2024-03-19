b6ca6c9e92b8255d43e541a2fb429351c492bf6f
GishB <SamofalovWORK@yandex.ru>
2024-03-20 00:19:24 +0300

    add a bit different sequence for sst_svd test

--- tests/test_SubspaceBased.py
+++ tests/test_SubspaceBased.py
@@ -13,9 +13,10 @@ class TestSingularSequenceTransformer:
     model = SingularSequenceTransformer()
     test_sequence = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     test_sequence_2 = np.array([111, 202, 903, 104, 205, 306, 707, 8, 9, 110])
+    test_sequence_3 = np.array([11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
     history_matrix = hankel(test_sequence)
     future_matrix = hankel(test_sequence_2)
-    future_matrix_2 = hankel(test_sequence)
+    future_matrix_2 = hankel(test_sequence_3)
     n_components = 1
 
     def test_get_hankel_matrix(self):
@@ -40,7 +41,7 @@ class TestSingularSequenceTransformer:
                                                         x_history=self.history_matrix,
                                                         n_components=self.n_components)
         assert distance_val_large > 0.05, "Large distance between two matrix in subspace."
-        assert distance_val_small < 1e-5, "Small distance between two identical matrix in subspace."
+        assert distance_val_small < 1e-2, "Small distance between two identical matrix in subspace."
 
 
 
